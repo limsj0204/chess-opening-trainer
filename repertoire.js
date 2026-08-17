3e3bab6367870b6a73f44a5071ed99ad631b4054
limsj0204 <limsj990204@gmail.com>
2026-08-18 08:18:01 +0900

    Expand repertoire with top opponent-reply variations (Fritz, Fried Liver, Bayonet Attack, Panov-Botvinnik, Bd2-Nxe4)

--- repertoire.js
+++ repertoire.js
@@ -10,12 +10,24 @@ const REPERTOIRE = {
       moves: "e4 e5 Nf3 Nc6 Bc4 Bc5 c3 Nf6 d4 exd4 cxd4 Bb4+ Nc3 Nxe4 O-O Bxc3 d5 Bf6 Re1 Ne7 Rxe4 d6 Bg5 Bxg5 Nxg5 h6",
     },
     {
-      name: "이탈리안 - 지오코 피아노 5.d4 조용한 라인 (7.Bd2)",
+      name: "이탈리안 - 지오코 피아노 5.d4 조용한 라인 (7.Bd2, 7...Bxd2+)",
       moves: "e4 e5 Nf3 Nc6 Bc4 Bc5 c3 Nf6 d4 exd4 cxd4 Bb4+ Bd2 Bxd2+ Nbxd2 d5 exd5 Nxd5 Qb3 Nce7 O-O O-O Rfe1 c6",
+    },
+    {
+      name: "이탈리안 - 지오코 피아노 5.d4 조용한 라인 (7.Bd2, 7...Nxe4)",
+      moves: "e4 e5 Nf3 Nc6 Bc4 Bc5 c3 Nf6 d4 exd4 cxd4 Bb4+ Bd2 Nxe4 Bxb4 Nxb4 Bxf7+ Kxf7 Qb3+ Kf8 Qxb4+ Qe7 Qxe7+ Kxe7",
     },
     {
       name: "투 나이츠 - 나이트 어택 (4.Ng5, 5...Na5 시고린 갬빗)",
       moves: "e4 e5 Nf3 Nc6 Bc4 Nf6 Ng5 d5 exd5 Na5 Bb5+ c6 dxc6 bxc6 Be2 h6 Nf3 e4 Ne5",
+    },
+    {
+      name: "투 나이츠 - 나이트 어택 (4.Ng5, 5...Nd4 프리츠 변형)",
+      moves: "e4 e5 Nf3 Nc6 Bc4 Nf6 Ng5 d5 exd5 Nd4 c3 b5 Bf1 Nxd5 cxd4 Qxg5 Bxb5",
+    },
+    {
+      name: "투 나이츠 - 나이트 어택 (4.Ng5, 5...Nxd5?! 프라이드 리버)",
+      moves: "e4 e5 Nf3 Nc6 Bc4 Nf6 Ng5 d5 exd5 Nxd5 Nxf7 Kxf7 Qf3+ Ke6 Nc3 Nb4",
     },
     {
       name: "이탈리안 게임 - 헝가리안 디펜스 (3...Be7)",
@@ -37,12 +49,20 @@ const REPERTOIRE = {
       moves: "e4 c6 d4 d5 Nc3 dxe4 Nxe4 Nf6 Nxf6+ exf6 c3 Bd6 Bd3 O-O Nf3 Re8+ Qe2 Bg4",
     },
     {
-      name: "카로칸 - 어드밴스 (3.e5)",
+      name: "카로칸 - 어드밴스 (3.e5, 4.Nf3 숏 시스템)",
       moves: "e4 c6 d4 d5 e5 Bf5 Nf3 e6 Be2 c5 O-O Nc6",
     },
     {
-      name: "카로칸 - 익스체인지 (3.exd5)",
+      name: "카로칸 - 어드밴스 (3.e5, 4.g4 베이오넷 어택)",
+      moves: "e4 c6 d4 d5 e5 Bf5 g4 Be4 f3 Bg6 h4 h5 Ne2 hxg4 fxg4 e6",
+    },
+    {
+      name: "카로칸 - 익스체인지 (3.exd5, 4.Bd3)",
       moves: "e4 c6 d4 d5 exd5 cxd5 Bd3 Nc6 c3 Nf6 Bf4 Bg4",
+    },
+    {
+      name: "카로칸 - 파노프-보트비닉 어택 (3.exd5 cxd5 4.c4)",
+      moves: "e4 c6 d4 d5 exd5 cxd5 c4 Nf6 Nc3 e6 Nf3 Be7 cxd5 Nxd5 Bd3 Nc6 O-O O-O Re1 Bf6",
     },
   ],
 };
